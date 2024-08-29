ecbff1965fceaab4caf4203039cd0c22d259685e
AngelAlx04 <Angelrap.2222@gmail.com>
2024-08-29 14:13:17 -0500

    Modificación Lista enlazada

--- ListasEnlazadas.js
+++ ListasEnlazadas.js
@@ -1,56 +1,106 @@
-class ListaEnlazada {
-    constructor(value, next = null) {
-      this.value = value;
-      this.next = next;
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
-  
-  function OrdenarLista(list1, list2) {
-    let mergedHead = null;
 
-    function insertarCabeza(node) {
-      node.next = mergedHead;
-      mergedHead = node;
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  // Método para agregar un nodo al final de la lista
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-  
-    while (list1 !== null && list2 !== null) {
-      if (list1.value >= list2.value) {
-        let next = list1.next;
-        insertarCabeza(list1);
-        list1 = next;
+  }
+
+  // Método para imprimir la lista
+  print() {
+    let node = this.head;
+    while (node != null) {
+      console.log(`Valor ${node.value} | Next: ${node.next?.value || null}`);
+      node = node.next;
+    }
+  }
+
+  // Método para fusionar dos listas y ordenarlas de menor a mayor
+  static mergeAndSort(list1, list2) {
+    const mergedList = new LinkedList();
+
+    let current1 = list1.head;
+    let current2 = list2.head;
+
+    while (current1 !== null && current2 !== null) {
+      if (current1.value >= current2.value) {
+        mergedList.append(current1.value);
+        current1 = current1.next;
       } else {
-        let next = list2.next;
-        insertarCabeza(list2);
-        list2 = next;
+        mergedList.append(current2.value);
+        current2 = current2.next;
       }
     }
-  
-    while (list1 !== null) {
-      let next = list1.next;
-      insertarCabeza(list1);
-      list1 = next;
+
+    // Si quedan elementos en list1
+    while (current1 !== null) {
+      mergedList.append(current1.value);
+      current1 = current1.next;
     }
-  
-    while (list2 !== null) {
-      let next = list2.next;
-      insertarCabeza(list2);
-      list2 = next;
+
+    // Si quedan elementos en list2
+    while (current2 !== null) {
+      mergedList.append(current2.value);
+      current2 = current2.next;
     }
-  
-    return mergedHead;
+
+    return LinkedList.reverseList(mergedList);
   }
 
-  function printList(head) {
-    let current = head;
+  // Método para invertir una lista enlazada
+  static reverseList(list) {
+    let prev = null;
+    let current = list.head;
+    let next = null;
+
     while (current !== null) {
-      console.log(current.value);
-      current = current.next;
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
     }
-  }
 
-  let list1 = new ListaEnlazada(5, new ListaEnlazada(3, new ListaEnlazada(1)));
-  let list2 = new ListaEnlazada(6, new ListaEnlazada(4, new ListaEnlazada(2)));
-  
-  let listaOrdenada = OrdenarLista(list1, list2);
-  printList(listaOrdenada);
-  
+    list.head = prev;
+
+    return list;
+  }
+}
+
+// Ejemplo de uso:
+const list1 = new LinkedList();
+list1.append(11);
+list1.append(7);
+list1.append(1);
+
+const list2 = new LinkedList();
+list2.append(6);
+list2.append(4);
+list2.append(2);
+
+console.log("Lista 1:");
+list1.print();
+
+console.log("Lista 2:");
+list2.print();
+
+const mergedList = LinkedList.mergeAndSort(list1, list2);
+console.log("Lista fusionada y ordenada:");
+mergedList.print();
