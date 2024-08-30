54d9cb0df10029455a5069d52b9efc48182d28a3
AngelAlx04 <Angelrap.2222@gmail.com>
2024-08-30 18:26:14 -0500

    Prueba

--- ListasEnlazadas.js
+++ ListasEnlazadas.js
@@ -65,7 +65,8 @@ class LinkedList {
     return LinkedList.reverseList(mergedList);
   }
 
-  // Método para invertir una lista enlazad
+  // Método para invertir una lista enlaz
+  
   static reverseList(list) {
     let prev = null;
     let current = list.head;
